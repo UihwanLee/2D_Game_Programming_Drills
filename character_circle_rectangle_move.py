0e7c0a8a885290ee0ce37dd583189c46802f8527
UihwanLee <luh5063014@naver.com>
2023-09-21 10:02:41 +0900

    소년과 잔디 캔버스에 그리기 성공

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -7,6 +7,15 @@ character = load_image('character.png')
 
 def run_circle():
     print('CIRCLE')
+
+    # 일단 그림을 그리자
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(400, 90)
+    delay(1)
+
+
+    
     pass
 
 def run_rectangle():
@@ -16,5 +25,6 @@ def run_rectangle():
 while True:
     run_circle()
     run_rectangle()
+    break
 
 close_canvas()
