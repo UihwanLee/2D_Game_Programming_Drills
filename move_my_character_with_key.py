f06a2d68092393dde80f111ae60479622bf3a85b
Lee Ui Hwan <luh5063014@naver.com>
2023-09-25 21:58:25 +0900

    anim_running 구현

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -72,7 +72,7 @@ def anim_IDLE():
 
 
 def anim_walking():
-    global  anim_frame_list
+    global anim_frame_list
 
     anim = anim_frame_list[1]
     frame = 0
@@ -81,12 +81,18 @@ def anim_walking():
         render_frame(frame, anim.left, anim.bottom, anim.width, anim.height, 90, 500, 100, 100, 0.2)
 
 def anim_running():
-    pass
+    global anim_frame_list
+
+    anim = anim_frame_list[2]
+    frame = 0
+
+    for frame in range(0, len(anim.left), 1):
+        render_frame(frame, anim.left, anim.bottom, anim.width, anim.height, 90, 500, 100, 100, 0.2)
 
 Init_Anim()
 while running:
     #anim_IDLE()
-    anim_walking()
+    #anim_walking()
     anim_running()
     handle_events()
 
