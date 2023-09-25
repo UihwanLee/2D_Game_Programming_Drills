6f98a8440065427c628c6586fa0dce908960c76c
UihwanLee <luh5063014@naver.com>
2023-09-25 18:25:35 +0900

    TUK_GROUND 배경 구현

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,7 +1,16 @@
 from pico2d import *
 
-open_canvas()
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+
 TUK_GROUND = load_image('TUK_GROUND.png')
 Sprite_Sheet = load_image('sprite_sheet.png')
+
+running = True
+
+while running:
+    clear_canvas()
+    TUK_GROUND.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    update_canvas()
 
 close_canvas()
