e74543d5b2a482703bb5b4e3a900b269d391967d
Lee Ui Hwan <luh5063014@naver.com>
2023-09-25 22:22:57 +0900

    방향키 이동에 따른 walking anim과 x축 이동 구현

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -15,6 +15,8 @@ class anim_frame():
 
 running = True
 anim_frame_list = []
+dir = 0
+pos_x = 100
 
 def Init_Anim():
     global anim_frame_list
@@ -44,14 +46,24 @@ def Init_Anim():
     anim_frame_list.append(anim_frame_running)
 
 def handle_events():
-    global running
+    global running, dir
 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+            elif event.key == SDLK_RIGHT:
+                dir += 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_RIGHT:
+                dir += 1
 
 def render_frame(frame, left, bottom, width, height, x, y, xScale, yScale, time):
     clear_canvas()
@@ -62,23 +74,26 @@ def render_frame(frame, left, bottom, width, height, x, y, xScale, yScale, time)
 
 
 def anim_IDLE():
-    global anim_frame_list
+    global anim_frame_list, pos_x
 
     anim = anim_frame_list[0]
     frame = 0
 
     for frame in range(0, len(anim.left), 1):
-        render_frame(frame, anim.left, anim.bottom, anim.width, anim.height, 90, 500, 100, 100, 0.5)
+        render_frame(frame, anim.left, anim.bottom, anim.width, anim.height, pos_x, 500, 100, 100, 0.5)
 
 
+
+frame_walking = 0
 def anim_walking():
-    global anim_frame_list
+    global anim_frame_list, frame_walking, pos_x, dir
 
     anim = anim_frame_list[1]
     frame = 0
 
-    for frame in range(0, len(anim.left), 1):
-        render_frame(frame, anim.left, anim.bottom, anim.width, anim.height, 90, 500, 100, 100, 0.2)
+    render_frame(frame_walking, anim.left, anim.bottom, anim.width, anim.height, pos_x, 500, 100, 100, 0.2)
+    frame_walking = (frame_walking + 1) % len(anim.left)
+    pos_x += dir * 20
 
 def anim_running():
     global anim_frame_list
@@ -92,8 +107,8 @@ def anim_running():
 Init_Anim()
 while running:
     #anim_IDLE()
-    #anim_walking()
-    anim_running()
+    anim_walking()
+    #anim_running()
     handle_events()
 
 close_canvas()
