9ea7c2846266113d65843a0da7d1fef5f5e78bbb
Lee Ui Hwan <luh5063014@naver.com>
2023-09-28 17:59:07 +0900

    handle_events 함수 삭제

--- move_character_chasing_random_mouse.py
+++ move_character_chasing_random_mouse.py
@@ -17,21 +17,6 @@ def generate_random_mouse():
     global mouse_points
     mouse_points = [random.randint(10, TUK_WIDTH-10), random.randint(10, TUK_HEIGHT-10)]
 
-def handle_events():
-    global running
-    global x, y
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
-    pass
-
-
-
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
@@ -44,9 +29,7 @@ while running:
     generate_random_mouse()
     mouse.draw(mouse_points[0], mouse_points[1])
     update_canvas()
-    delay(2)
+    delay(1)
     #frame = (frame + 1) % 8
-
-    handle_events()
 
 close_canvas()
