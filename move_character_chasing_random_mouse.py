326da75cfe996964d3c30535a3002086ad875153
Lee Ui Hwan <luh5063014@naver.com>
2023-09-28 17:58:34 +0900

    mouse 랜덤 위치로 이동하기 구현

--- move_character_chasing_random_mouse.py
+++ move_character_chasing_random_mouse.py
@@ -1,3 +1,5 @@
+import random
+
 from pico2d import *
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
@@ -6,6 +8,14 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 mouse = load_image('hand_arrow.png')
+
+# 글로벌 변수
+mouse_points = []
+
+# 마우스 무작위 위치 생성
+def generate_random_mouse():
+    global mouse_points
+    mouse_points = [random.randint(10, TUK_WIDTH-10), random.randint(10, TUK_HEIGHT-10)]
 
 def handle_events():
     global running
@@ -31,9 +41,11 @@ while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     #character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
-    mouse.draw(x, y)
+    generate_random_mouse()
+    mouse.draw(mouse_points[0], mouse_points[1])
     update_canvas()
-    frame = (frame + 1) % 8
+    delay(2)
+    #frame = (frame + 1) % 8
 
     handle_events()
 
