75a237990a46d1f9c1263ad3659ef17a7498fb25
UihwanLee <luh5063014@naver.com>
2023-10-05 10:54:47 +0900

    world 리스트를 만들어 모든 객체를 담고 그리기 구현

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,5 +1,5 @@
 from pico2d import *
-import  random
+import random
 
 
 # Game object class here
@@ -16,7 +16,7 @@ class Grass:
 class Boy:
     def __init__(self):
         self.x, self.y = random.randint(100, 700), 90
-        self.frame = 0
+        self.frame = random.randint(0, 7)
         self.image = load_image('run_animation.png')
 
     def update(self):
@@ -41,24 +41,27 @@ def reset_world():
     global running
     global grass
     global team
+    global world
 
     running = True
+    world = []
+
     grass = Grass()
+    world.append(grass)
+
     team = [Boy() for i in range(11)]
+    world += team
 
 
 def update_world():
-    grass.update()
-    for boy in team:
-        boy.update()
-    pass
+    for o in world:
+        o.update()
 
 
 def render_world():
     clear_canvas()
-    grass.draw()
-    for boy in team:
-        boy.draw()
+    for o in world:
+        o.draw()
     update_canvas()
 
 
