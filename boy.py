cb372cf7dbf3ac4f50ee746034e79a1dfd01ea1d
UihwanLee <luh5063014@naver.com>
2023-10-12 13:10:13 +0900

    AutoRun state 동안 캐릭터 크기 2배로 커지기 구현

--- boy.py
+++ boy.py
@@ -55,7 +55,7 @@ class AutoRun:
 
     @staticmethod
     def draw(boy):
-        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y + 30, 200, 200)
         pass
 
 
