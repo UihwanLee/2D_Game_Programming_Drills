16bc8336cd05b09c130abbee322e66e3be93cf7c
UihwanLee <luh5063014@naver.com>
2023-10-19 10:22:01 +0900

    Ball 객체가 화면 밖으로 나갈 때 객체 삭제 구현

--- game_world.py
+++ game_world.py
@@ -4,8 +4,9 @@
 objects = []
 
 # 월드에 객체에 넣는 함수
-def add(o):
+def add_object(o):
     objects.append(o)
+
 
 # 월드를 업데이트하는, 객체들을 모두 업데이트하는 함수
 def update():
@@ -16,3 +17,7 @@ def update():
 def render():
     for o in objects:
         o.draw()
+
+# 객체 삭제
+def remove_object(o):
+    objects.remove(o)
