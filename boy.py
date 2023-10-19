86378da0533e63f7b3403322fc16ea4a33abeeec
UihwanLee <luh5063014@naver.com>
2023-10-19 09:55:28 +0900

    SPACE 키를 눌렀을 때 fire_ball() 함수 호출 하는 상태 변환 구현

--- boy.py
+++ boy.py
@@ -45,6 +45,8 @@ class Idle:
 
     @staticmethod
     def exit(boy, e):
+        if space_down(e):
+            boy.fire_ball()
         pass
 
     @staticmethod
@@ -114,9 +116,9 @@ class StateMachine:
         self.boy = boy
         self.cur_state = Idle
         self.transitions = {
-            Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep},
-            Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
-            Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle}
+            Idle: {space_down: Idle, right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep},
+            Run: {space_down: Run, right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
+            Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run}
         }
 
     def start(self):
@@ -161,3 +163,7 @@ class Boy:
 
     def draw(self):
         self.state_machine.draw()
+
+
+    def fire_ball(self):
+        print('FIRE BALL')
