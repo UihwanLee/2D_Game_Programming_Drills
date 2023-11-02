23b804ae6be5fa4f52fb6746fe8a12398cd35ac4
UihwanLee <luh5063014@naver.com>
2023-11-02 10:25:39 +0900

    소년의 시간 당 속도를 시간 당 픽셀 이동 값으로 변환하고 time을 font를 구현 완료.

--- boy.py
+++ boy.py
@@ -35,7 +35,11 @@ def time_out(e):
 
 
 # Boy Run Speed
-# fill here
+PIXEL_PER_METER = (10.0 / 0.3)
+RUN_SPEED_KMPH = 20.0
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
 # Boy Action Speed
 # fill here
@@ -99,8 +103,7 @@ class Run:
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + 1) % 8
-        boy.x += boy.dir * 5
-        boy.x = clamp(25, boy.x, 1600-25)
+        boy.x += boy.dir * RUN_SPEED_PPS * game_framework.frame_time
 
 
     @staticmethod
@@ -180,6 +183,7 @@ class Boy:
         self.state_machine = StateMachine(self)
         self.state_machine.start()
         self.item = 'Ball'
+        self.font = load_font('ENCR10B.TTF', 16)
 
 
     def fire_ball(self):
@@ -206,3 +210,4 @@ class Boy:
 
     def draw(self):
         self.state_machine.draw()
+        self.font.draw(self.x - 60, self.y + 50, f'(Time:{get_time():.2f})', (255, 255, 0))
