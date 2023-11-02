916208ca311bf30aa69dc728505fa7db24da21d5
UihwanLee <luh5063014@naver.com>
2023-11-02 10:43:50 +0900

    액션 프레임 또한 시간에 관한 계산 구현 완료.

--- boy.py
+++ boy.py
@@ -1,9 +1,10 @@
 # 이것은 각 상태들을 객체로 구현한 것임.
 
-from pico2d import get_time, load_image, load_font, clamp,  SDL_KEYDOWN, SDL_KEYUP, SDLK_SPACE, SDLK_LEFT, SDLK_RIGHT
+from pico2d import get_time, load_image, load_font, clamp, SDL_KEYDOWN, SDL_KEYUP, SDLK_SPACE, SDLK_LEFT, SDLK_RIGHT
 from ball import Ball, BigBall
 import game_world
 import game_framework
+
 
 # state event check
 # ( state event type, event value )
@@ -23,15 +24,16 @@ def left_down(e):
 def left_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
 
+
 def space_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
 
 def time_out(e):
     return e[0] == 'TIME_OUT'
 
+
 # time_out = lambda e : e[0] == 'TIME_OUT'
-
-
 
 
 # Boy Run Speed
@@ -42,15 +44,9 @@ RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
 # Boy Action Speed
-# fill here
-
-
-
-
-
-
-
-
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
 
 
 class Idle:
@@ -63,7 +59,7 @@ class Idle:
             boy.action = 3
         boy.dir = 0
         boy.frame = 0
-        boy.wait_time = get_time() # pico2d import 필요
+        boy.wait_time = get_time()  # pico2d import 필요
         pass
 
     @staticmethod
@@ -74,23 +70,22 @@ class Idle:
 
     @staticmethod
     def do(boy):
-        boy.frame = (boy.frame + 1) % 8
+        boy.frame = (boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
         if get_time() - boy.wait_time > 2:
             boy.state_machine.handle_event(('TIME_OUT', 0))
 
     @staticmethod
     def draw(boy):
-        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
-
+        boy.image.clip_draw(int(boy.frame) * 100, boy.action * 100, 100, 100, boy.x, boy.y)
 
 
 class Run:
 
     @staticmethod
     def enter(boy, e):
-        if right_down(e) or left_up(e): # 오른쪽으로 RUN
+        if right_down(e) or left_up(e):  # 오른쪽으로 RUN
             boy.dir, boy.action, boy.face_dir = 1, 1, 1
-        elif left_down(e) or right_up(e): # 왼쪽으로 RUN
+        elif left_down(e) or right_up(e):  # 왼쪽으로 RUN
             boy.dir, boy.action, boy.face_dir = -1, 0, -1
 
     @staticmethod
@@ -102,14 +97,12 @@ class Run:
 
     @staticmethod
     def do(boy):
-        boy.frame = (boy.frame + 1) % 8
+        boy.frame = (boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
         boy.x += boy.dir * RUN_SPEED_PPS * game_framework.frame_time
-
 
     @staticmethod
     def draw(boy):
-        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
-
+        boy.image.clip_draw(int(boy.frame) * 100, boy.action * 100, 100, 100, boy.x, boy.y)
 
 
 class Sleep:
@@ -126,8 +119,6 @@ class Sleep:
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + 1) % 8
-
-
 
     @staticmethod
     def draw(boy):
@@ -169,9 +160,6 @@ class StateMachine:
         self.cur_state.draw(self.boy)
 
 
-
-
-
 class Boy:
     def __init__(self):
         self.x, self.y = 400, 90
@@ -185,14 +173,13 @@ class Boy:
         self.item = 'Ball'
         self.font = load_font('ENCR10B.TTF', 16)
 
-
     def fire_ball(self):
 
-        if self.item ==   'Ball':
-            ball = Ball(self.x, self.y, self.face_dir*10)
+        if self.item == 'Ball':
+            ball = Ball(self.x, self.y, self.face_dir * 10)
             game_world.add_object(ball)
         elif self.item == 'BigBall':
-            ball = BigBall(self.x, self.y, self.face_dir*10)
+            ball = BigBall(self.x, self.y, self.face_dir * 10)
             game_world.add_object(ball)
         # if self.face_dir == -1:
         #     print('FIRE BALL LEFT')
